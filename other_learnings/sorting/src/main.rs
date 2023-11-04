b6a40467d4902d7da3966b1fbdad4d6d3287b032
HornAdam8 <AdamHorn@ADT.COM>
2023-11-03 22:24:35 -0600

    adding sorting

--- other_learnings/sorting/src/main.rs
+++ other_learnings/sorting/src/main.rs
@@ -1,5 +1,7 @@
 use rand::Rng;
-use std::time::SystemTime;
+use std::collections::HashMap;
+use std::fmt::Display;
+use std::time::{Duration, SystemTime};
 macro_rules! is_sorted {
     ($data:expr) => {
         is_sorted_pred($data, |x, y| x < y)
@@ -9,65 +11,170 @@ macro_rules! is_sorted {
     };
 }
 
+struct Result {
+    name: String,
+    time: Duration,
+    sorted: bool,
+}
+
 fn main() {
-    let size = 25000;
-    println!("n size is {size}");
+    let mut size = 1000;
+    let mut results: HashMap<usize, Vec<Result>> = HashMap::default();
+    let mut radix_valid = true;
+    let mut quick_valid = true;
+    let mut block_valid = true;
+    let mut merge_valid = true;
+    let mut shell_valid = true;
+    let mut insertion_valid = true;
+    let mut selection_valid = true;
+    let mut bubble_valid = true;
+    while radix_valid
+        || quick_valid
+        || block_valid
+        || merge_valid
+        || shell_valid
+        || insertion_valid
+        || selection_valid
+        || bubble_valid
+    {
+        let mut my_vec1 = gen_rand_vec(size);
+        let mut my_vec2 = my_vec1.clone();
+        let mut my_vec3 = my_vec1.clone();
+        let mut my_vec4 = my_vec1.clone();
+        let mut my_vec5 = my_vec1.clone();
+        let mut my_vec6 = my_vec1.clone();
+        let mut my_vec7 = my_vec1.clone();
+        let mut my_vec8 = my_vec1.clone();
 
-    let mut my_vec1 = gen_rand_vec(size);
-    let mut my_vec2 = my_vec1.clone();
-    let mut my_vec3 = my_vec1.clone();
-    let mut my_vec4 = my_vec1.clone();
-    let mut my_vec5 = my_vec1.clone();
-    let mut my_vec6 = my_vec1.clone();
+        if radix_valid {
+            let start = SystemTime::now();
+            radix_sort(&mut my_vec1);
+            let elapsed = start.elapsed().unwrap();
+            results.insert(
+                size,
+                vec![Result {
+                    name: "radix sort".to_string(),
+                    time: elapsed,
+                    sorted: is_sorted!(my_vec1),
+                }],
+            );
+            if elapsed.as_secs() >= 60 {
+                radix_valid = false;
+            }
+        }
 
-    let start = SystemTime::now();
-    radix_sort(&mut my_vec1);
-    let elapsed = start.elapsed().unwrap();
-    println!(
-        "radix sort took {elapsed:?}   sorted: {}",
-        is_sorted!(my_vec1)
-    );
+        if quick_valid {
+            let start = SystemTime::now();
+            quicksort(&mut my_vec2);
+            let elapsed = start.elapsed().unwrap();
+            results.get_mut(&size).unwrap().push(Result {
+                name: "quicksort".to_string(),
+                time: elapsed,
+                sorted: is_sorted!(my_vec2),
+            });
+            if elapsed.as_secs() >= 60 {
+                quick_valid = false;
+            }
+        }
 
-    let start = SystemTime::now();
-    quicksort(&mut my_vec2);
-    let elapsed = start.elapsed().unwrap();
-    println!(
-        "quick sort took {elapsed:?}   sorted: {}",
-        is_sorted!(my_vec2)
-    );
+        if block_valid {
+            let start = SystemTime::now();
+            block_sort(&mut my_vec3);
+            let elapsed = start.elapsed().unwrap();
+            results.get_mut(&size).unwrap().push(Result {
+                name: "block sort".to_string(),
+                time: elapsed,
+                sorted: is_sorted!(my_vec3),
+            });
+            if elapsed.as_secs() >= 60 {
+                block_valid = false;
+            }
+        }
 
-    let start = SystemTime::now();
-    block_sort(&mut my_vec3);
-    let elapsed = start.elapsed().unwrap();
-    //println!("{my_vec2:?}");
-    println!(
-        "block sort took {elapsed:?}   sorted: {}",
-        is_sorted!(my_vec3)
-    );
+        if merge_valid {
+            let start = SystemTime::now();
+            mergesort(&mut my_vec4);
+            let elapsed = start.elapsed().unwrap();
+            results.get_mut(&size).unwrap().push(Result {
+                name: "mergesort".to_string(),
+                time: elapsed,
+                sorted: is_sorted!(my_vec4),
+            });
+            if elapsed.as_secs() >= 60 {
+                merge_valid = false;
+            }
+        }
 
-    let start = SystemTime::now();
-    mergesort(&mut my_vec4);
-    let elapsed = start.elapsed().unwrap();
-    println!(
-        "merge sort took {elapsed:?}   sorted: {}",
-        is_sorted!(my_vec4)
-    );
+        if shell_valid {
+            let start = SystemTime::now();
+            shell_sort(&mut my_vec5);
+            let elapsed = start.elapsed().unwrap();
+            results.get_mut(&size).unwrap().push(Result {
+                name: "shell sort".to_string(),
+                time: elapsed,
+                sorted: is_sorted!(my_vec5),
+            });
+            if elapsed.as_secs() >= 60 {
+                shell_valid = false;
+            }
+        }
 
-    let start = SystemTime::now();
-    insertion_sort(&mut my_vec5);
-    let elapsed = start.elapsed().unwrap();
-    println!(
-        "insertion sort took {elapsed:?}   sorted: {}",
-        is_sorted!(my_vec5)
-    );
+        if insertion_valid {
+            let start = SystemTime::now();
+            insertion_sort(&mut my_vec6);
+            let elapsed = start.elapsed().unwrap();
+            results.get_mut(&size).unwrap().push(Result {
+                name: "insertion sort".to_string(),
+                time: elapsed,
+                sorted: is_sorted!(my_vec6),
+            });
+            if elapsed.as_secs() >= 60 {
+                insertion_valid = false;
+            }
+        }
 
-    let start = SystemTime::now();
-    bubble_sort(&mut my_vec6);
-    let elapsed = start.elapsed().unwrap();
-    println!(
-        "bubble sort took {elapsed:?}   sorted: {}",
-        is_sorted!(my_vec6)
-    );
+        if selection_valid {
+            let start = SystemTime::now();
+            selection_sort(&mut my_vec7);
+            let elapsed = start.elapsed().unwrap();
+            results.get_mut(&size).unwrap().push(Result {
+                name: "selection sort".to_string(),
+                time: elapsed,
+                sorted: is_sorted!(my_vec7),
+            });
+            if elapsed.as_secs() >= 60 {
+                selection_valid = false;
+            }
+        }
+
+        if bubble_valid {
+            let start = SystemTime::now();
+            bubble_sort(&mut my_vec8);
+            let elapsed = start.elapsed().unwrap();
+            results.get_mut(&size).unwrap().push(Result {
+                name: "bubble sort".to_string(),
+                time: elapsed,
+                sorted: is_sorted!(my_vec8),
+            });
+            if elapsed.as_secs() >= 60 {
+                bubble_valid = false;
+            }
+        }
+
+        let v = results.get_mut(&size).unwrap();
+        v.sort_by_key(|e| e.time);
+        let mut place = 1;
+        println!("\n\nn size: {size}");
+        for rec in v {
+            println!(
+                "{place}. {}: {:?}, sorted: {}",
+                rec.name, rec.time, rec.sorted
+            );
+            place += 1;
+        }
+
+        size *= 2;
+    }
 }
 
 fn radix_sort(v: &mut Vec<i32>) {
@@ -196,8 +303,12 @@ fn block_sort(v: &mut Vec<i32>) {
     let mut blocks: Vec<Vec<i32>> = vec![];
     let mut handles = vec![];
     let div = v.len() / 16;
-    for _ in 0..16 {
+    for i in 0..16 {
         let mut block: Vec<i32> = v.drain(0..div).collect();
+        if i == 15 {
+            let mut rem: Vec<i32> = v.drain(0..).collect();
+            block.append(&mut rem);
+        }
         handles.push(std::thread::spawn(|| {
             mergesort(&mut block);
             block
@@ -237,6 +348,43 @@ fn insertion_sort(v: &mut Vec<i32>) {
     }
 }
 
+fn shell_sort(v: &mut Vec<i32>) {
+    let mut gap_vals = vec![];
+    let mut val = 1;
+    let mut pow = 2;
+    while val < v.len() {
+        gap_vals.push(val);
+        val = 2_usize.pow(pow) - 1;
+        pow += 1;
+    }
+    for val in gap_vals {
+        for i in 0..val {
+            insertion_sort_interleaved(v, i, val);
+        }
+    }
+}
+fn insertion_sort_interleaved(v: &mut Vec<i32>, start: usize, gap: usize) {
+    for i in (start + gap..v.len()).step_by(gap) {
+        let mut j = i;
+        while j >= gap && (j - gap) >= start && v[j] < v[j - gap] {
+            v.swap(j, j - gap);
+            j -= gap;
+        }
+    }
+}
+
+fn selection_sort(v: &mut Vec<i32>) {
+    for i in 0..v.len() - 1 {
+        let mut smallest = i;
+        for j in i + 1..v.len() {
+            if v[j] < v[smallest] {
+                smallest = j;
+            }
+        }
+        v.swap(i, smallest);
+    }
+}
+
 fn gen_rand_vec(size: usize) -> Vec<i32> {
     let mut ret = vec![];
     for _ in 0..size {
@@ -246,10 +394,11 @@ fn gen_rand_vec(size: usize) -> Vec<i32> {
     ret
 }
 
-fn is_sorted_pred<T: Ord, F: Fn(&T, &T) -> bool>(v: Vec<T>, pred: F) -> bool {
+fn is_sorted_pred<T: Ord + Display, F: Fn(&T, &T) -> bool>(v: Vec<T>, pred: F) -> bool {
     let mut last = v.get(0).unwrap();
     for i in 1..v.len() {
         if pred(v.get(i).unwrap(), last) {
+            println!("current = {}, last = {last}", v.get(i).unwrap());
             return false;
         }
         last = &v[i];
